7124816c39345b0bae8c1a18dbd7ac59ea12b26a
Sebastien Rosset (serosset) <serosset@cisco.com>
2021-01-04 14:24:54 -0800

    dependency test

--- tools.go
+++ tools.go
@@ -1,4 +1,4 @@
-package _
+package mod1
 
 //go:generate sh -c "mkdir -p worker && /bin/echo -n $'package worker\n\nimport \"fmt\"\n\nfunc Run() {\n\tfmt.Println(\"Running\")\n}\n' > worker/worker.go"
 
